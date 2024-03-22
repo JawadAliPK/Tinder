0f948f5100e89fe8e4465362ee78ffb2dd084da0
Jawad Ali <jawad@eddress.co>
2024-03-22 16:41:46 +0100

    Initial Commit

---
+++ Tinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tinder
+//
+//  Created by jawad ali on 22/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by jawad ali on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

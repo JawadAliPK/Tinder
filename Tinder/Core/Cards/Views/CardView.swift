00e936c2ce1062ed3f044b67447f09c07e5f519c
Jawad Ali <jawad@eddress.co>
2024-03-24 13:38:18 +0100

    Swipping Card Animation complete

--- Tinder/Core/Cards/Views/CardView.swift
+++ Tinder/Core/Cards/Views/CardView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CardView: View {
     
+    @State private var xOffSet: CGFloat = 0
+    @State private var degrees: Double = 0
     
     
     var body: some View {
@@ -23,14 +25,36 @@ struct CardView: View {
         }
         .frame(width: cardWidth, height: cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .offset(x: xOffSet)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy, value: xOffSet)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged)
+                .onEnded(onDragEnded)
+        )
     }
 }
 
-#Preview {
-    CardView()
+private extension CardView {
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffSet = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
+        
+        if abs(width) <= abs(screenCutoff) {
+            xOffSet = 0
+            degrees = 0
+        }
+    }
 }
 
 private extension CardView {
+    private var screenCutoff: CGFloat {
+        (UIScreen.main.bounds.width / 2) * 0.8
+    }
     private var cardWidth: CGFloat {
         UIScreen.main.bounds.width - 20
     }
@@ -38,4 +62,8 @@ private extension CardView {
     private var cardHeight: CGFloat {
         UIScreen.main.bounds.height / 1.45
     }
+}
+
+#Preview {
+    CardView()
 }
